d0641d681ed218dcc51524afe0c56eb128f1fe0d
root <root@ip-172-31-14-172.sa-east-1.compute.internal>
2025-06-08 12:21:28 +0000

    updated tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,2 @@
-import app.main
-
 def test_example():
     assert 1 == 1
-
-
-def test_index_route():
-    app = app.main.app.test_client()
-    response = app.get('/')
-    assert response.status_code == 200
-    assert b"Hello from raman-micro!" in response.data
