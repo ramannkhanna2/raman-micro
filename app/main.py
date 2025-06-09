4048b5410eecc13fce6d130a8bdc60276b3a8264
root <root@ip-172-31-14-172.sa-east-1.compute.internal>
2025-06-09 09:12:02 +0000

    changed output

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello from raman-microApp!"
+    return "Hello from raman-micro"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
